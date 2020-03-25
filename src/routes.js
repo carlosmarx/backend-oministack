3142e7ed2c2ccefd20a4efbff1ee17aba650ab3f
Carlos Marx <carlosmarx27@gmail.com>
2020-03-25 17:24:40 -0300

    Ajuste estrutura, configure controller, login,cadastro e listagem de ongs e casos, cors

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,22 @@
-const express = require('express')
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
+routes.post('/session', SessionController.create);
 
-    var body = request.body;
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
 
-    console.log(body)
+routes.get('/profile', ProfileController.index);
 
-    return response.json({
-        event: 'Semana ominStack 11.0',
-        aluno: 'Manoel Carlos'
-    })
-});
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
+
 
 module.exports = routes;
